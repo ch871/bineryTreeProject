9956f43ffa38f79476a4c769eb146df8e58015f5
chaim <shterenchaim@gmail.com>
2024-09-05 17:47:48 +0300

    finisht project

--- bineryTreeProject/BSTree.cs
+++ bineryTreeProject/BSTree.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using bineryTreeProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ByneriTry
+namespace bineryTreeProject
 {
     internal class TreeNode
     {
-        public List<string>? defense {  get; set; }
-        public (int,int) Range { get; set; }
+        public List<string>? Defense {  get; set; }
+        public (int min,int max) Value { get; set; }
+        public int height = -1;
         public TreeNode? Left { get; set; }
         public TreeNode? Right { get; set; }
 
     }
     internal class BSTree
     {
-        private TreeNode? root;
+        public TreeNode? root;
 
         public bool Search(int value)
         {
@@ -33,13 +35,13 @@ namespace ByneriTry
             }
 
             // If the value is found, return true
-            if (value == node.Value)
+            if (value >= node.Value.min && value <= node.Value.max)
             {
                 return true;
             }
 
             // If the value is less than the current node, search in the left subtree
-            if (value < node.Value)
+            if (value < node.Value.min)
             {
                 return SearchRecursive(node.Left, value);
             }
@@ -50,13 +52,13 @@ namespace ByneriTry
             }
         }
 
-        public void Remove(int value)
+        public void Remove((int min, int max) value)
         {
             // Use a helper method for the recursive implementation
             root = RemoveRecursive(root, value);
         }
 
-        private TreeNode? RemoveRecursive(TreeNode? node, int value)
+        private TreeNode? RemoveRecursive(TreeNode? node, (int min,int max)value)
         {
             // Base case: if the node is null, the value is not in the tree
             if (node == null)
@@ -65,12 +67,12 @@ namespace ByneriTry
             }
 
             // If the value is less than the current node, recurse left
-            if (value < node.Value)
+            if (value.max < node.Value.min)
             {
                 node.Left = RemoveRecursive(node.Left, value);
             }
             // If the value is greater than the current node, recurse right
-            else if (value > node.Value)
+            else if (value.min > node.Value.max)
             {
                 node.Right = RemoveRecursive(node.Right, value);
             }
@@ -96,7 +98,7 @@ namespace ByneriTry
                 else
                 {
                     // Find the minimum value in the right subtree (successor)
-                    int minValue = FindMin(node.Right);
+                    (int min, int max) minValue = FindMin(node.Right);
                     // Replace the current node's value with the successor's value
                     node.Value = minValue;
                     // Remove the successor from the right subtree
@@ -106,9 +108,7 @@ namespace ByneriTry
 
             return node;
         }
-
-
-        private int FindMin(TreeNode node)
+        public static (int min,int max) FindMin(TreeNode node)
         {
             while (node.Left != null)
             {
@@ -116,36 +116,51 @@ namespace ByneriTry
             }
             return node.Value;
         }
+        public static (int min, int max) FindMax(TreeNode node)
+        {
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+            return node.Value;
+        }
 
-        public void Insert(int value)
+        public void Insert(DefencModel value)
         {
             // Use a helper method for the recursive implementation
             root = InsertRecursive(root, value);
         }
 
 
-        private TreeNode? InsertRecursive(TreeNode? node, int value)
+        private TreeNode? InsertRecursive(TreeNode? node, DefencModel value)
         {
             // Base case: if the node is null, create a new node with the given value
             if (node == null)
             {
-                return new TreeNode() { Value = value };
+                return new TreeNode()
+                {
+                    Value = (min: value.MinSeverity, max: value.MaxSeverity),
+                    Defense = value.Defenses,
+                    height = 0
+                };
             }
 
             // If the value already exists, don't insert it again
-            if (value == node.Value)
+            if (value.MaxSeverity == node.Value.max)
             {
                 return node;
             }
 
             // Recursively insert into the left subtree if the value is smaller
-            if (value < node.Value)
+            if (value.MaxSeverity < node.Value.min)
             {
+                node.height += 1;
                 node.Left = InsertRecursive(node.Left, value);
             }
             // Recursively insert into the right subtree if the value is larger
             else
             {
+                node.height += 1;
                 node.Right = InsertRecursive(node.Right, value);
             }
 
@@ -153,9 +168,9 @@ namespace ByneriTry
             return node;
         }
 
-        public List<int> InOrderTraversal() => InOrderTraversalHelper(root);
+        public List<string> InOrderTraversal() => InOrderTraversalHelper(root);
 
-        private List<int> InOrderTraversalHelper(TreeNode? node)
+        private List<string> InOrderTraversalHelper(TreeNode? node)
         {
             // Base case: if the current node is null, return an empty list
             if (node == null)
@@ -167,7 +182,7 @@ namespace ByneriTry
             var leftSubtreeList = InOrderTraversalHelper(node.Left);
 
             // Create a list with the current node's value
-            var currentNodeList = new List<int> { node.Value };
+            var currentNodeList =new List<string> { node.Defense.Aggregate(string.Empty, (str, next) => str + "," + next) };
 
             // Recursively get the list from the right subtree
             var rightSubtreeList = InOrderTraversalHelper(node.Right);
@@ -176,15 +191,15 @@ namespace ByneriTry
             return [.. leftSubtreeList, .. currentNodeList, .. rightSubtreeList];
         }
 
-        public List<int> PreOrderTraversal() => PreOrderTraversalHelper(root);
+        public List<string> PreOrderTraversal() => PreOrderTraversalHelper(root);
 
-        private List<int> PreOrderTraversalHelper(TreeNode? node)
+        private List<string> PreOrderTraversalHelper(TreeNode? node)
         {
             // Base case: if the current node is null, return an empty list
             if (node == null) { return []; }
 
             // Create a list with the current node's value
-            var currentNodeList = new List<int> { node.Value };
+            var currentNodeList = new List<string> { node.Defense.Aggregate(string.Empty, (str, next) => str + "," + next) };
 
             // Recursively get the list from the left subtree
             var leftSubtreeList = PreOrderTraversalHelper(node.Left);
--- bineryTreeProject/Program.cs
+++ bineryTreeProject/Program.cs
@@ -1,10 +1,107 @@
-﻿namespace bineryTreeProject
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace bineryTreeProject
 {
     internal class Program
     {
+        public static List<ThretModel>? Threats { get; set; }
+        public static List<DefencModel>? DefenceStrategies { get; set; }
+        public static BSTree DefenceTree { get; set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Threats = jsonTools.ReadFromJsonAsync<List<ThretModel>>("../../../threats.json");
+            DefenceStrategies = jsonTools.ReadFromJsonAsync<List<DefencModel>>("../../../defenceStrategiesBalanced.json");
+
+            DefenceTree = InsertDefences(DefenceStrategies, new BSTree());
+
+            PrintTree(DefenceTree.root,"root");
+
+            findDefencPreOrderHelper(culcThretValeu(Threats[0]), DefenceTree.root);
+        }
+        public static BSTree InsertDefences(List<DefencModel> defenceStrategies, BSTree tree)
+        {
+            foreach (DefencModel def in defenceStrategies)
+            {
+                tree.Insert(def);
+            }
+            return tree;
+        }
+        public static void PrintTree(TreeNode root , string LorRorRoot)
+        {
+
+            if (root != null)
+            {
+                string str = new string('-', root.height) +root.Value.ToString()+string.Join("," , root.Defense);
+                // Print the current value
+                Console.WriteLine(str);
+                // Traverse left subtree
+                PrintTree(root.Left,"left");
+                // Traverse right subtree
+                PrintTree(root.Right,"right");
+            }
+        }        
+        public static int culcThretValeu(ThretModel thret)
+        {
+            int TargetValue;
+            switch (thret.Target)
+            {
+
+                case "Web Server":
+                    TargetValue = 10;
+                    break;
+                case "Database":
+                    TargetValue = 15;
+                    break;
+                case "User Credentials":
+                    TargetValue = 20;
+                    break;
+                default:
+                    TargetValue = 5;
+                    break;
+            }
+            int severity = (thret.Volume * thret.Sophistication) + TargetValue;
+            return severity;
+        }
+        public static void findDefencPreOrderHelper(int threatValeu, TreeNode? root)
+        {
+            if (threatValeu < BSTree.FindMin(root).min)
+            {
+                Console.WriteLine("Atec Server is bloe the threshold. Attack is ignored");
+                return;
+            }
+            if (threatValeu > BSTree.FindMax(root).max)
+            {
+                Console.WriteLine("No suitable defence was found. Brace for impact");
+            }
+            else
+            {
+                findDefencPreOrder(threatValeu, root);
+            }
+        }
+        private static void findDefencPreOrder(int threatValeu , TreeNode? root)
+        {
+            // Base case: if the current node is null, return an empty list
+            if (root == null) { return; }
+        
+            if (threatValeu >= root.Value.min && threatValeu <= root.Value.max)
+            {
+                for(int index =0; index < root.Defense.Count(); index++)
+                {
+                    Console.WriteLine(root.Defense[index]);
+                    Thread.Sleep(2000);
+                    return;
+                };
+            }
+
+            // Recursively get the list from the left subtree
+            findDefencPreOrder(threatValeu ,root.Left);
+
+            // Recursively get the list from the right subtree
+             findDefencPreOrder(threatValeu,root.Right);
+
         }
     }
 }
